f57cbaca6125cf348c69b4fa7aec94b29806da0f
Sverre Nystad <sverrenystad@gmail.com>
2025-11-07 12:39:23 +0100

    feat: add training script for YOLO-obb model with W&B integration

---
+++ train-obb.py
@@ -0,0 +1,26 @@
+from ultralytics import YOLO
+from dotenv import load_dotenv
+import os
+import wandb
+
+load_dotenv()
+
+wandb.login(key=os.getenv("WANDB_API_KEY"))
+wandb.init(project="sport-tracking")
+
+
+model = YOLO("yolo11l.pt")
+
+model.train(
+    data="/cluster/work/sverrnys/sport-tracking/rbk_all.yaml",
+    epochs=100,
+    imgsz=1280,
+    batch=16,
+    workers=8,
+    project="runs/train",
+    name="rbk_all",
+)
+
+model.val()
+
+model.export(format="onnx")
